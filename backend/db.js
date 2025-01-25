616f688e273d4027a4e5eed2701f94675e2bd09b
coco <tldjs3651@naver.com>
2025-01-26 00:26:18 +0900

    update backend'

---
+++ backend/db.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const mysql = require("mysql2/promise");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "coco0000",
+  database: "mydb",
+});
+
+module.exports = db;
